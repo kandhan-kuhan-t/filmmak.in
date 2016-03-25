e37abec9e800596dc2fd5cc26ed92dd51812d2ff
kandhan <kandhan.kuhan@gmail.com>
2016-03-26 00:03:30 +0530

    filmak.sql updated to videos table

--- filmak.sql
+++ filmak.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 25, 2016 at 07:09 PM
+-- Generation Time: Mar 26, 2016 at 12:01 AM
 -- Server version: 5.6.28-0ubuntu0.15.10.1
 -- PHP Version: 5.6.11-1ubuntu3.1
 
@@ -40,6 +40,28 @@ INSERT INTO `users` (`username`, `password`, `name`) VALUES
 ('kandha', '123', 'k'),
 ('kuhan', '123', 'kuhan');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE IF NOT EXISTS `videos` (
+  `username` varchar(30) DEFAULT NULL,
+  `videoID` varchar(30) NOT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  `description` varchar(30) DEFAULT NULL,
+  `genre` varchar(30) DEFAULT NULL,
+  `duration` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `videos`
+--
+
+INSERT INTO `videos` (`username`, `videoID`, `title`, `description`, `genre`, `duration`) VALUES
+('kuhan', 'yFTtFkXPug8', '', '', '', 'PT46S');
+
 --
 -- Indexes for dumped tables
 --
@@ -49,6 +71,12 @@ INSERT INTO `users` (`username`, `password`, `name`) VALUES
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`videoID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

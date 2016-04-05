298644e8ed188ec6bcac0d47edc760b636d5c8a6
kandhan <kandhan.kuhan@gmail.com>
2016-04-05 19:59:22 +0530

    filmak.sql added

---
+++ filmak.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.13.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 05, 2016 at 07:57 PM
+-- Server version: 5.6.28-0ubuntu0.15.10.1
+-- PHP Version: 5.6.11-1ubuntu3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `filmak`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `casting`
+--
+
+CREATE TABLE IF NOT EXISTS `casting` (
+  `videoID` varchar(50) NOT NULL,
+  `member_name` varchar(100) NOT NULL,
+  `field` varchar(50) NOT NULL,
+  `isMember` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE IF NOT EXISTS `contact` (
+  `username` varchar(50) NOT NULL,
+  `contact_number` varchar(20) NOT NULL,
+  `email_id` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `profile_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_profile`
+--
+
+CREATE TABLE IF NOT EXISTS `users_profile` (
+  `profile_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `about` text NOT NULL,
+  `experience` varchar(100) NOT NULL,
+  `field` varchar(30) NOT NULL,
+  `birth_date` int(11) NOT NULL,
+  `birth_year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `users_profile`
+--
+DELIMITER $$
+CREATE TRIGGER `below_insert_insert_into_contact` BEFORE INSERT ON `users_profile`
+ FOR EACH ROW insert into contact
+set username = new.username
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE IF NOT EXISTS `videos` (
+  `username` varchar(30) NOT NULL DEFAULT '',
+  `videoID` varchar(30) NOT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  `description` varchar(30) DEFAULT NULL,
+  `genre` varchar(30) DEFAULT NULL,
+  `duration` varchar(30) DEFAULT NULL,
+  `views` int(11) DEFAULT '0',
+  `upload_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`username`,`contact_number`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `users_profile`
+--
+ALTER TABLE `users_profile`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`username`,`videoID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
